6de48dab9825ddd30d28190aa72280c97a8e9287
Layefa7 <139487973+Layefa7@users.noreply.github.com>
2023-11-01 19:15:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,19 +4,19 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 //═══════[Required Variables]════════\\
 global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
+global.mongodb = process.env.MONGODB_URI || "mongodb://uufpxy0mfyeykbrxkjpk:m1b0dsDODVFWlBy3AF3@bufirembtiksxw6ni25m-mongodb.services.clever-cloud.com:2533/bufirembtiksxw6ni25m"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/Layefa7/RIKA-MD'
+global.github = 'https://github.com/Godsoulsolos/Soul-MD'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '994408223019'
-global.devs = '994408223019';
-global.website = 'https://github.com/Layefa7/RIKA-MD' //wa.me/+2349070616688
+global.sudo = process.env.SUDO || '2349070616688'
+global.devs = '2349070616688';
+global.website = 'https://github.com/Godsoulsolos/Soul-MD' //wa.me/+2349070616688
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'Soul-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'rika' : process.env.OWNER_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'soul' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
   author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'rika' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
@@ -44,7 +44,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'RIKA',
+  LANG: process.env.THEME|| 'SOUL',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
 
