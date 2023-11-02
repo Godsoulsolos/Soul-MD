5a79e5f248ce3fd3b75cba7cfb108212e5a2fc6f
Godsoulsolos <143340497+Godsoulsolos@users.noreply.github.com>
2023-11-02 11:33:18 +0100

    Update _pm2.js

--- Commands/_pm2.js
+++ Commands/_pm2.js
@@ -10,9 +10,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Rika-Md
- * @author : Rika <https://github.com/Layefa 7>
- * @description : Rika,A Multi-functional whatsapp bot.
+ * @project_name : Soul-MD
+ * @author : Looneyffkun <https://github.com/Godsoulsolos>
+ * @description : Soul,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
